a30754e2bc958ce9c0fefa9b4c24118a80d4cc09
dashwanth_ram <ramchowdary640@gmail.com>
2025-11-13 18:46:27 +0530

    1

--- frontend/src/app/services/expense.service.ts
+++ frontend/src/app/services/expense.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment'; // correct path
 
 
 export interface Expense {
